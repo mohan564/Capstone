7afb80223a1da5bc0d1d030a89046edc95597200
Mohan <mkrishan564@gmail.com>
2018-10-31 19:13:21 +0530

    Commit after five images fetched from Firebase

--- app/src/main/java/com/example/harpreet/vasdapunjab/Whats_New.java
+++ app/src/main/java/com/example/harpreet/vasdapunjab/Whats_New.java
@@ -29,11 +29,12 @@ import java.io.File;
 import java.io.IOException;
 
 public class Whats_New extends Fragment {
-
+    
     FirebaseStorage storage;
     StorageReference reference;
-    ImageView image_1;
-    TextView title_1,detail_1;
+    ImageView image_1,image_2,image_3,image_4,image_5;
+    TextView title_1,title_2,title_3,title_4,title_5;
+    TextView detail_1,detail_2,detail_3,detail_4,detail_5;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -54,6 +55,24 @@ public class Whats_New extends Fragment {
         image_1 = view.findViewById(R.id.image_1);
         title_1 = view.findViewById(R.id.title_1);
         detail_1 = view.findViewById(R.id.details_1);
+
+        image_2 = view.findViewById(R.id.image_2);
+        title_2 = view.findViewById(R.id.title_2);
+        detail_2 = view.findViewById(R.id.details_2);
+
+        image_3 = view.findViewById(R.id.image_3);
+        title_3 = view.findViewById(R.id.title_3);
+        detail_3 = view.findViewById(R.id.details_3);
+
+        image_4 = view.findViewById(R.id.image_4);
+        title_4 = view.findViewById(R.id.title_4);
+        detail_4 = view.findViewById(R.id.details_4);
+
+        image_5 = view.findViewById(R.id.image_5);
+        title_5 = view.findViewById(R.id.title_5);
+        detail_5 = view.findViewById(R.id.details_5);
+
+
     }
 
     private void updateImages() {
@@ -66,7 +85,8 @@ public class Whats_New extends Fragment {
         storage = FirebaseStorage.getInstance();
         reference = storage.getReferenceFromUrl("gs://preuniprep-master.appspot.com");
 
-        StorageReference pathreference = reference.child("chips_shitake.jpg");
+        //1 image fetch karn layi code
+        StorageReference pathreference = reference.child("image1.jpg");
 
         File myFile = null;
 
@@ -81,17 +101,136 @@ public class Whats_New extends Fragment {
         pathreference.getFile(myFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
             @Override
             public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                Toast.makeText(getActivity(), "Done G", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getActivity(), "Image 1 fetched", Toast.LENGTH_SHORT).show();
                 Bitmap bitmap = BitmapFactory.decodeFile(finalMyFile.getAbsolutePath());
                 image_1.setImageBitmap(bitmap);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(), "Image 1 not done", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        //2 image fetch karn layi code
+
+        StorageReference pathreference2 = reference.child("image2.png");
+
+        File myFile2 = null;
+
+        try {
+            myFile2 = File.createTempFile("images","jpg");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final File finalMyFile2 = myFile2;
+        pathreference2.getFile(myFile2).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getActivity(), "Image 2 fetched", Toast.LENGTH_SHORT).show();
+                Bitmap bitmap2 = BitmapFactory.decodeFile(finalMyFile2.getAbsolutePath());
+                image_2.setImageBitmap(bitmap2);
                 dialog.dismiss();
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getActivity(), "Nahi 22 hoea ni", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "Image 2 not done", Toast.LENGTH_SHORT).show();
                 dialog.dismiss();
             }
         });
+
+        //3 image fetch karn layi code
+
+        StorageReference pathreference3 = reference.child("image3.png");
+
+        File myFile3 = null;
+
+        try {
+            myFile3 = File.createTempFile("images","jpg");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final File finalMyFile3 = myFile3;
+        pathreference3.getFile(myFile3).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getActivity(), "Image 3 fetched", Toast.LENGTH_SHORT).show();
+                Bitmap bitmap3 = BitmapFactory.decodeFile(finalMyFile3.getAbsolutePath());
+                image_3.setImageBitmap(bitmap3);
+                dialog.dismiss();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(), "Image 3 not done", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        //4 image fetch karn layi code
+        StorageReference pathreference4 = reference.child("image4.png");
+
+        File myFile4 = null;
+
+        try {
+            myFile4 = File.createTempFile("images","jpg");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final File finalMyFile4 = myFile4;
+        pathreference4.getFile(myFile4).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getActivity(), "Image 4 fetched", Toast.LENGTH_SHORT).show();
+                Bitmap bitmap4 = BitmapFactory.decodeFile(finalMyFile4.getAbsolutePath());
+                image_4.setImageBitmap(bitmap4);
+                dialog.dismiss();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(), "Image 4 not done", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        //5 image fetch karn layi code
+        StorageReference pathreference5 = reference.child("image5.jpg");
+
+        File myFile5 = null;
+
+        try {
+            myFile5 = File.createTempFile("images","jpg");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final File finalMyFile5 = myFile5;
+        pathreference5.getFile(myFile5).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getActivity(), "Image 5 fetched", Toast.LENGTH_SHORT).show();
+                Bitmap bitmap5 = BitmapFactory.decodeFile(finalMyFile5.getAbsolutePath());
+                image_5.setImageBitmap(bitmap5);
+                dialog.dismiss();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(), "Image 5 not done", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+
     }
 }
