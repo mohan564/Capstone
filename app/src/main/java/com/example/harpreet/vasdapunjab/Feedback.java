4fe0200b38895525ef884c6231a9aecb8015e7a8
harpreetmultani9614@gmail.com <harpreetmultani9614@gmail.com>
2018-11-21 14:20:50 -0800

    Merge branch 'master' of C:\Users\harpr\AndroidStudioProjects\capstone with conflicts.

--- app/src/main/java/com/example/harpreet/vasdapunjab/Feedback.java
+++ app/src/main/java/com/example/harpreet/vasdapunjab/Feedback.java
@@ -6,15 +6,23 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Feedback extends Fragment {
+
+
+    WebView webView ;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.feedback,container,false);
 
-
+        webView = (WebView) view.findViewById(R.id.webviewFeedBack);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.youtube.com/watch?v=TUXui5ItBkM");
         return view;
     }
 }
